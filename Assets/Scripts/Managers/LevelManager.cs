3bf5741fc68ce7eb9e8c8a4de33069b038586cfa
unknown <alphaceves@gmail.com>
2024-12-17 12:24:27 -0600

    completed working pool level system

--- Assets/Scripts/Managers/LevelManager.cs
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -1,7 +1,6 @@
 using UnityEngine;
 using System.Collections;
-using System.IO;
-using Unity.VisualScripting;
+using System.Collections.Generic;
 
 namespace Managers
 {
@@ -10,6 +9,11 @@ namespace Managers
         public Level[] levelList;
         public Transform levelLocation;
         public GameObject poolLevel;
+        public Transform cameraPosition;
+        private List<Transform> activeObstacles = new();
+        private bool isAllowed;
+        private float disableRange = -30f;
+        private float enableRange = 30f;
         private void OnEnable()
         {
             GameManager.GameStart += HandleGameStart;
@@ -32,34 +36,74 @@ namespace Managers
             switch (option)
             {
                 case 1:
-                    foreach (Level level in levelList)
-                    {
-                        StartCoroutine(SetObstacles(level));
-                    }
+                    StartCoroutine(SetObstacles());
+                    StartCoroutine(ObstacleManager());
                     break;
                 case 2:
+                    StopAllCoroutines();
                     break;
                 default:
                     break;
             }
         }
 
-        private IEnumerator SetObstacles(Level level)
+        private IEnumerator SetObstacles()
         {
-            for (int i = 0; i < level.obstacleCount; i++)
+            foreach (Level level in levelList)
             {
-                GameObject randomObstacle = level.obstacleList[Random.Range(0, level.obstacleList.Length)];
+                for (int i = 0; i < level.obstacleCount; i++)
+                {
+                    isAllowed = false;
+                    activeObstacles.Add(levelLocation);
+                    yield return new WaitUntil(() => isAllowed);
+                    GameObject randomObstacle = level.obstacleList[Random.Range(0, level.obstacleList.Length)];
 
-                GameObject currentObstacle = LevelPool(randomObstacle);
-                yield return new WaitUntil(() => currentObstacle.GetComponent<Obstacle_1>().obstacleEndPosition != null);
-                levelLocation = currentObstacle.GetComponent<Obstacle_1>().obstacleEndPosition;
+                    GameObject currentObstacle = LevelPool(randomObstacle);
+                    yield return new WaitUntil(() => currentObstacle.GetComponent<Obstacle_1>().obstacleEndPosition != null);
+                    levelLocation = currentObstacle.GetComponent<Obstacle_1>().obstacleEndPosition;
+                }
             }
             yield return null;
         }
 
+        private IEnumerator ObstacleManager()
+        {
+            while (true)
+            {
+                yield return new WaitUntil(() => activeObstacles.Count > 0);
+
+                for (int i = 0; i < activeObstacles.Count; i++)
+                {
+                    if (i == activeObstacles.Count - 1 && !isAllowed)
+                    {
+                        if (levelLocation.position.z - cameraPosition.position.z < enableRange)
+                        {
+                            isAllowed = true;
+                        }
+                    }
+                    else
+                    {
+                        if (activeObstacles[i].position.z - cameraPosition.position.z < disableRange)
+                        {
+                            if (activeObstacles[i].parent != null)
+                            {
+                                activeObstacles[i].parent.gameObject.SetActive(false);
+                                activeObstacles.Remove(activeObstacles[i]);
+                            }
+                            else
+                            {
+                                activeObstacles[i].gameObject.SetActive(false);
+                                activeObstacles.Remove(activeObstacles[i]);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
         private GameObject LevelPool(GameObject wantedObject)
         {
-            Transform[] obstacleList = poolLevel.GetComponentsInChildren<Transform>();
+            Transform[] obstacleList = poolLevel.GetComponentsInChildren<Transform>(true);
             Vector3 obstaclePosition = new(levelLocation.position.x, levelLocation.position.y, levelLocation.position.z + (levelLocation.localScale.z / 2));
 
             foreach (Transform obstacle in obstacleList)
